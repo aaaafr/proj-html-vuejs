c7b180a6e73de5b4524eb233a698d0feb1c3eae7
aaaafr <anastasia.frassini@icloud.com>
2021-06-15 14:14:15 +0200

    header

--- dist/js/app.js
+++ dist/js/app.js
@@ -8,6 +8,24 @@
 /***/ (() => {
 
 alert("hello");
+var app = new Vue({
+  el: '#app',
+  data: {
+    navbar: [{
+      link: "Home"
+    }, {
+      link: "Order Online"
+    }, {
+      link: "About"
+    }, {
+      link: "News"
+    }, {
+      link: "Contact Us"
+    }]
+  },
+  methods: {},
+  mounted: function mounted() {}
+});
 
 /***/ }),
 
--- src/app.js
+++ src/app.js
@@ -1,1 +1,33 @@
 alert("hello");
+
+var app = new Vue({
+    el: '#app',
+    data: {
+      navbar: [
+          {
+              link: "Home",
+
+          },
+          {
+              link: "Order Online",
+          },
+          {
+              link: "About",
+
+          },
+          {
+              link: "News",
+          },
+          {
+              link:"Contact Us"
+          },
+
+      ]
+    },
+    methods: {
+
+    },
+    mounted(){
+
+    }
+  })
