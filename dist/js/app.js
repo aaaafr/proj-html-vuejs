f1bcd5bcd7c487708073d5cf8d769e47b6de8898
aaaafr <anastasia.frassini@icloud.com>
2021-06-17 12:34:38 +0200

    footer complete

--- dist/js/app.js
+++ dist/js/app.js
@@ -12,8 +12,6 @@ var app = new Vue({
   el: '#app',
   data: {
     navbar: [{
-      link: "Home"
-    }, {
       link: "Order Online"
     }, {
       link: "About"
@@ -75,7 +73,7 @@ var root = new Vue({
       nome: 'Privacy Policy'
     }],
     days: [{
-      nome: 'OPENING Hours'
+      nome: 'OPENING HOURS'
     }, {
       nome: 'Mon -',
       hour: '10AM to 11PM'
@@ -102,86 +100,7 @@ var root = new Vue({
       nome: 'VOUCHER'
     }]
   }
-}); //               },
-//               {
-//                 title: 'NAVIGATE',
-//                 menu: [
-//                     {
-//                         nome:'Home',
-//                     },
-//                     {
-//                         nome:'Alternate Home',
-//                     },
-//                     {
-//                         nome:'Menu',
-//                     },
-//                     {
-//                         nome:'About',
-//                     },
-//                     {
-//                         nome:'News',
-//                     },
-//                     {
-//                         nome:'Contact Us',
-//                     },
-//                     {
-//                         nome:'Cart',
-//                     },
-//                 ]
-//               },
-//               {
-//                 title: 'INFORMATION',
-//                 info:[
-//                     {
-//                         nome:'My Account',
-//                     },
-//                     {
-//                         nome:'Terms Of Service',
-//                     },
-//                     {
-//                         nome:'Privacy Policy',
-//                     },
-//                 ]
-//               },
-//               {
-//                 title: 'OPENING HOURS',
-//                 days:[
-//                     {
-//                         nome:'Mon -',
-//                         hour:'10AM to 11PM'
-//                     },
-//                     {
-//                         nome:'Tue -',
-//                         hour:'10AM to 11PM'
-//                     },
-//                     {
-//                         nome:'Wed -',
-//                         hour:'10AM to 11PM'
-//                     },
-//                     {
-//                         nome:'Thur -',
-//                         hour:'10AM to 11PM'
-//                     },
-//                     {
-//                         nome:'Fri -',
-//                         hour:'10AM to 11PM'
-//                     },
-//                     {
-//                         nome:'Sat -',
-//                         hour:'10AM to 1AM'
-//                     },
-//                     {
-//                         nome:'Sun -',
-//                         hour:'11AM to 10PM'
-//                     },
-//                 ]
-//               },
-//               {
-//                 title: 'VOUCHER'
-//               }
-//           ]
-//       }
-//   })
+});
 
 /***/ }),
 
--- src/app.js
+++ src/app.js
@@ -5,10 +5,6 @@ var app = new Vue({
     data: {
       navbar: [
           {
-              link: "Home",
-
-          },
-          {
               link: "Order Online",
           },
           {
@@ -106,7 +102,7 @@ var app = new Vue({
         ],
         days: [
             {
-                nome: 'OPENING Hours',
+                nome: 'OPENING HOURS',
             },
             {
                 nome: 'Mon -',
@@ -147,86 +143,3 @@ var app = new Vue({
 
 })
             
-//               },
-//               {
-//                 title: 'NAVIGATE',
-//                 menu: [
-//                     {
-//                         nome:'Home',
-//                     },
-//                     {
-//                         nome:'Alternate Home',
-//                     },
-//                     {
-//                         nome:'Menu',
-//                     },
-//                     {
-//                         nome:'About',
-//                     },
-//                     {
-//                         nome:'News',
-//                     },
-//                     {
-//                         nome:'Contact Us',
-//                     },
-//                     {
-//                         nome:'Cart',
-//                     },
-
-//                 ]
-//               },
-//               {
-//                 title: 'INFORMATION',
-//                 info:[
-//                     {
-//                         nome:'My Account',
-//                     },
-//                     {
-//                         nome:'Terms Of Service',
-//                     },
-//                     {
-//                         nome:'Privacy Policy',
-//                     },
-
-//                 ]
-//               },
-//               {
-//                 title: 'OPENING HOURS',
-//                 days:[
-//                     {
-//                         nome:'Mon -',
-//                         hour:'10AM to 11PM'
-//                     },
-//                     {
-//                         nome:'Tue -',
-//                         hour:'10AM to 11PM'
-//                     },
-//                     {
-//                         nome:'Wed -',
-//                         hour:'10AM to 11PM'
-//                     },
-//                     {
-//                         nome:'Thur -',
-//                         hour:'10AM to 11PM'
-//                     },
-//                     {
-//                         nome:'Fri -',
-//                         hour:'10AM to 11PM'
-//                     },
-//                     {
-//                         nome:'Sat -',
-//                         hour:'10AM to 1AM'
-//                     },
-//                     {
-//                         nome:'Sun -',
-//                         hour:'11AM to 10PM'
-//                     },
-
-//                 ]
-//               },
-//               {
-//                 title: 'VOUCHER'
-//               }
-//           ]
-//       }
-//   })
