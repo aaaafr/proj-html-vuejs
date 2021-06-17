0e078cd8c64c68507853a9045b70f3f6296889f1
aaaafr <anastasia.frassini@icloud.com>
2021-06-17 10:25:58 +0200

    footer link

--- src/app.js
+++ src/app.js
@@ -33,44 +33,119 @@ var app = new Vue({
   })
 
   const root = new Vue({
-      el:'#root',
-      data:{
-          footer:[
-              {
-                title: 'ORDER ONLINE',
-              },
-              {
-                plate:[
-                    {
-                        nome: 'Appetizers'
-                    },
-                    {
-                        nome: 'Burgers'
-                    },
-                    {
-                        nome: 'Pizza'
-                    },
-                    {
-                        nome: 'Fries'
-                    },
-                    {
-                        nome: 'Sides'
-                    },
-                    {
-                        nome: 'Desserts'
-                    },
-                    {
-                        nome: 'Beverages'
-                    },
-                    {
-                        nome: 'Specials'
-                    },
-                ]
+    el:'#root',
+    data:{
+        plates:[
+            {
+                nome: 'ORDER ONLINE',
+            },
+            {
+                nome: 'Appetizers'
+            },
+            {
+                nome: 'Burgers'
+            },
+            {
+                nome: 'Pizza'
+            },
+            {
+                nome: 'Fries'
+            },
+            {
+                nome: 'Sides'
+            },
+            {
+                nome: 'Desserts'
+            },
+            {
+                nome: 'Beverages'
+            },
+            {
+                nome: 'Specials'
+            },
+        ],
+        navigate: [
+            {
+                nome: 'NAVIGATE',
+            },
+            {
+                nome: 'Home'
+            },
+            {
+                nome: 'Alternate Home'
+            },
+            {
+                nome: 'Menu'
+            },
+            {
+                nome: 'About'
+            },
+            {
+                nome: 'News'
+            },
+            {
+                nome: 'Contact Us'
+            },
+            {
+                nome: 'Cart'
+            },
+        ],
+        info: [
+            {
+                nome: 'INFORMATION',
+            },
+            {
+                nome: 'My Account'
+            },
+            {
+                nome: 'Terms Of Service'
+            },
+            {
+                nome: 'Privacy Policy'
+            },
+        ],
+        days: [
+            {
+                nome: 'OPENING Hours',
+            },
+            {
+                nome: 'Mon -',
+                hour:'10AM to 11PM'
+            },
+            {
+                nome: 'Tue -',
+                hour:'10AM to 11PM'
+            },
+            {
+                nome: 'Wed -',
+                hour:'10AM to 11PM'
+            },
+            {
+                nome: 'Thur -',
+                hour:'10AM to 11PM'
+            },
+            {
+                nome: 'Fri -',
+                hour:'10AM to 11PM'
+            },
+            {
+                nome: 'Sat -',
+                hour:'10AM to 1AM'
+            },
+            {
+                nome: 'Sun -',
+                hour:'11AM to 10PM'
+            },
+        ],
+        vouch: [
+            {
+                nome: 'VOUCHER'
+            }
+        ]
 
-              }
-            ]
-        }
-    })
+    },
+
+})
             
 //               },
 //               {
